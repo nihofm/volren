7d020fd7a21c6c21a1dc709db88dd039546bdd36
Nikolai Hofmann <nikolai.hofmann@fau.de>
2021-05-28 02:47:35 +0200

    convergence shader update

--- shader/convergence.fs
+++ shader/convergence.fs
@@ -9,7 +9,7 @@ float luma(const vec3 col) { return dot(col, vec3(0.212671f, 0.715160f, 0.072169
 
 vec3 heatmap(const float val) {
     const float hue = 251.1 / 360.f; // blue
-    const vec3 hsv = vec3(hue + clamp(val, 0.f, 1.f) * -hue, 1, val < 1e-4 ? 0 : 1); // from blue to red
+    const vec3 hsv = vec3(hue + clamp(val, 0.f, 1.f) * -hue, 1, 1); // from blue to red
     // map hsv to rgb
     const vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     const vec3 p = abs(fract(vec3(hsv.x) + vec3(K)) * 6.f - vec3(K.w));
@@ -19,6 +19,6 @@ vec3 heatmap(const float val) {
 void main() {
     vec3 col = texture(color, tc).rgb;
     vec3 eve = texture(even, tc).rgb;
-    out_col = vec4(abs(col - eve) / max(1e-2, luma(col)), 1);
-    out_col.rgb = heatmap(luma(out_col.rgb));
+    out_col = vec4(abs(col - eve) / max(1e-6, sqrt(luma(col))), 1);
+    out_col.rgb = heatmap(5 * luma(out_col.rgb));
 }
